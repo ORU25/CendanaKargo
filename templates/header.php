3dd727901868b4700df40242abb79b624332b39a
Haykal Makhmud <haikal252005@gmail.com>
2025-10-14 03:45:58 +0800

    fix: unauthorized redirect bug

--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= isset($title) ? $title : 'Cendana Kargo'; ?></title>
     <?php
-    define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/CendanaKargo/');
+    if(!defined('BASE_URL')){
+        define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/CendanaKargo/');
+    }
     ?>
     <link rel="stylesheet" href="<?= BASE_URL; ?>Bootstrap/css/bootstrap.min.css">
 </head>
